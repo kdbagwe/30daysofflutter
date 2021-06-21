20bf6f53b30664c5d75383067f9e8ad0f5b75c23
Kaustubh Bagwe <kdbagwe@gmail.com>
2021-06-21 21:10:38 +0530

    day 2

---
+++ lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext) {
+    
+    var day = 30;
+    var name = "Kaustubh's";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("$name App")
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $day Days of flutter app")
+        )
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
